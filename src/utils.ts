1f15afa799193c3d103b3f7092beee81fb906faa
Johan <johan@macair2020>
2023-04-01 14:21:53 +0200

    build + test

--- src/utils.ts
+++ src/utils.ts
@@ -1,21 +1,21 @@
-import fs from 'fs';
+import fs from "fs";
 
-export function readFileToBuffer(filePath: string): Promise<Buffer> {
+export const readFileToBuffer = (filePath: string): Promise<Buffer> => {
   return new Promise((resolve, reject) => {
     const readStream = fs.createReadStream(filePath);
     const chunks: Buffer[] = [];
 
-    readStream.on('data', (chunk: Buffer) => {
+    readStream.on("data", (chunk: Buffer) => {
       chunks.push(chunk);
     });
 
-    readStream.on('end', () => {
+    readStream.on("end", () => {
       const buffer = Buffer.concat(chunks);
       resolve(buffer);
     });
 
-    readStream.on('error', err => {
+    readStream.on("error", (err) => {
       reject(err);
     });
   });
-}
+};
