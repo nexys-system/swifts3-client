19b1269e786785c24eb601541d71eccd41d7ce60
Johan <johan@macair2020>
2023-04-03 22:35:53 +0200

    fix test issue

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+// Sync object
+const config = {
+  preset: "ts-jest/presets/js-with-ts-esm",
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "js"],
+  transform: {
+    "^.+\\.(t|j)s$": [
+      "ts-jest",
+      {
+        /* ts-jest config goes here in Jest */
+      },
+    ],
+  },
 };
+
+export default config;
