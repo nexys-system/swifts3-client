f9d371d727cbf81828ab9800ef49bfd764ed6b30
John B <johan@nexys.ch>
2023-09-02 03:01:07 +0200

    Update object-repository.ts

--- src/object-repository.ts
+++ src/object-repository.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import fetch, { Response } from "node-fetch";
-import Identity from "./identity.js";
-import { readFileToBuffer } from "./utils.js";
+import Identity from "./identity";
+import { readFileToBuffer } from "./utils";
 
 class SwiftFileStorage {
   url: string;
